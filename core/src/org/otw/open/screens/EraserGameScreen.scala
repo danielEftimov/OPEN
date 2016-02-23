25557480f5b7678e0ba8f92d1d5993aab666ad0e
eilievska <elizabeta.ilievska@gmail.com>
2016-02-23 16:23:50 +0100

    Added eraser shape cursor on the level 1 game.

--- core/src/org/otw/open/screens/EraserGameScreen.scala
+++ core/src/org/otw/open/screens/EraserGameScreen.scala
@@ -1,11 +1,16 @@
 package org.otw.open.screens
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Pixmap
 import org.otw.open.actors.{BackgroundActor, ErasableImageActor}
 
 /**
   * Created by eilievska on 2/12/2016.
   */
 class EraserGameScreen() extends AbstractGameScreen {
+
+  private val clickCursor = Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("eraser-3.png")), 25, 50)
+  Gdx.graphics.setCursor(clickCursor)
 
   override def buildStage(): Unit = {
     addActor(new BackgroundActor("dark-background.png"))
