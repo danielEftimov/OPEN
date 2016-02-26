4f084ada23ce1cf6965ecee04c811ae128312616
eilievska <elizabeta.ilievska@gmail.com>
2016-02-26 16:39:50 +0100

    Load cursor from user settings.

--- desktop/src/org/otw/open/desktop/DesktopLauncher.java
+++ desktop/src/org/otw/open/desktop/DesktopLauncher.java
@@ -3,6 +3,7 @@ package org.otw.open.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.otw.open.OpenGame;
+import org.otw.open.controllers.ScreenController;
 import org.otw.open.util.UserSettings;
 
 public class DesktopLauncher {
@@ -14,8 +15,8 @@ public class DesktopLauncher {
         config.height = 900;
         config.resizable = true;
         config.title = "OPEN - a children's game";
-        UserSettings.setUserSettings(arg[0],arg[1],arg[2]);
+        UserSettings.setUserSettings(arg[0], arg[1], arg[2]);
         new LwjglApplication(OpenGame.getGame(), config);
-
+        ScreenController.setUpCursor();
     }
 }
