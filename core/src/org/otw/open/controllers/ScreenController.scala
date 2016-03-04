abeb07fbb207d779b505d0c8fc3041490b7165b5
deftimov <Daniel.Eftimov@endava.com>
2016-03-04 10:51:55 +0100

    ADD yellow pointers in all sizes on both B&W and color versions.

--- core/src/org/otw/open/controllers/ScreenController.scala
+++ core/src/org/otw/open/controllers/ScreenController.scala
@@ -18,7 +18,6 @@ object ScreenController {
   private val clickCursor = UserSettings.pointerSize match {
     case "s" => Gdx.graphics.newCursor(getCursorPixmap, 26, 4)
     case "m" => Gdx.graphics.newCursor(getCursorPixmap, 52, 7)
-    //case "l" => Gdx.graphics.newCursor(getCursorPixmap, 104, 15)
   }
 
   /**
@@ -71,7 +70,6 @@ object ScreenController {
     UserSettings.pointerSize match {
       case "s" => 64
       case "m" => 128
-      //case "l" => 256
     }
   }
 
